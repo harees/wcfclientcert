739f2cd2a455f0d97161051d45eac6d4f67b7ccf
Harees <hareesseni@gmail.com>
2021-07-07 01:13:59 -0500

    Final code

---
+++ WCFCertificateTest/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Configuration;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Threading.Tasks;
+using WCFCertificateTest.ServiceReference1;
+
+namespace WCFCertificateTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<IService1Channel> factory
+                = new ChannelFactory<IService1Channel>("BasicHttpsBinding_IService1");
+            factory.Credentials.ClientCertificate.SetCertificate(StoreLocation.CurrentUser, StoreName.My, X509FindType.FindBySubjectName, "hsyed");
+            IService1Channel wcfClientChannel = factory.CreateChannel(); 
+            
+           var data = wcfClientChannel.GetData(1);
+
+        }
+
+    }
+    public class MessageEndpointBehavior : IEndpointBehavior
+    {
+        public void AddBindingParameters(ServiceEndpoint endpoint, System.ServiceModel.Channels.BindingParameterCollection bindingParameters)
+        {
+            // No implementation necessary  
+        }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            clientRuntime.MessageInspectors.Add(new MessageInspector());
+        }
+
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+            // No implementation necessary  
+        }
+
+        public void Validate(ServiceEndpoint endpoint)
+        {
+            // No implementation necessary  
+        }
+    }
+
+    public class MessageInspector : IClientMessageInspector
+    {
+        public void AfterReceiveReply(ref System.ServiceModel.Channels.Message reply, object correlationState)
+        {
+            
+        }
+
+        public object BeforeSendRequest(ref System.ServiceModel.Channels.Message request, IClientChannel channel)
+        {
+            var userName = Environment.UserName;
+            HttpRequestMessageProperty httpRequestMessage;
+
+            if (request.Properties.TryGetValue(HttpRequestMessageProperty.Name, out var httpRequestMessageObject))
+            {
+                httpRequestMessage = httpRequestMessageObject as HttpRequestMessageProperty;
+                if (httpRequestMessage != null && string.IsNullOrEmpty(httpRequestMessage.Headers["X-User"]))
+                {
+                    httpRequestMessage.Headers["X-User"] = userName;
+                }
+            }
+            else
+            {
+                httpRequestMessage = new HttpRequestMessageProperty();
+                httpRequestMessage.Headers.Add("X-User", userName);
+                request.Properties.Add(HttpRequestMessageProperty.Name, httpRequestMessage);
+            }
+            return null;
+        }
+    }
+    public class MessageBehaviorExtensionElement : BehaviorExtensionElement
+    {
+        public override Type BehaviorType => typeof(MessageEndpointBehavior);
+
+        protected override object CreateBehavior()
+        {
+            // Create the  endpoint behavior that will insert the message  
+            // inspector into the client runtime  
+            return new MessageEndpointBehavior();
+        }
+    }
+
+
+}
---
+++ WCFService/MyValidator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Selectors;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.Web;
+
+namespace WCFService
+{
+    public static class X509CertificateExtensions
+    {
+        /// <summary>
+        /// Extracts Username from X509Certificate2 certificate.
+        /// </summary>
+        /// <param name="certificate">X509Certificate2</param>
+        /// <returns></returns>
+        public static string ExtractCommonName(this X509Certificate2 certificate)
+        {
+            if (certificate == null) return string.Empty;
+
+            foreach (string str in certificate.Subject.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (!str.Trim().StartsWith("CN", StringComparison.InvariantCultureIgnoreCase)) continue;
+
+                int i = str.IndexOf("=", StringComparison.InvariantCultureIgnoreCase) + 1;
+                if (i > 0 && str.Length >= i)
+                {
+                    return str.Substring(i).Trim();
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+
+    public class MyValidator: X509CertificateValidator
+    {
+        private const string ArrCertificateHeaderKey = "X-ARR-ClientCert";
+        private const string ClientCertificateHeaderKey = "X-Client-Certificate";
+        private const string Sha1HeaderKey = "X-SSL-Client-SHA1";
+        private const string SslClientVerifyHeader = "X-SSL-Client-Verify";
+        private const string SslClientCnHeaderKey = "X-SSL-Client-CN";
+        public override void Validate(X509Certificate2 certificate)
+        {
+            var subjectCert = certificate;
+            if (subjectCert == null)
+            {
+                    string certHeader = HttpContext.Current.Request.Headers[ArrCertificateHeaderKey];
+                    if (string.IsNullOrWhiteSpace(certHeader))
+                        certHeader = HttpContext.Current.Request.Headers[ClientCertificateHeaderKey];
+                    var mesageHeaders = OperationContext.Current.IncomingMessageHeaders;
+                    if (!string.IsNullOrWhiteSpace(certHeader))
+                    {
+                        var clientCertBytes = Convert.FromBase64String(certHeader);
+                        subjectCert = new X509Certificate2(clientCertBytes);
+                    }
+            }
+            var username = HttpContext.Current.Request.Headers["X-USER"];
+            if (subjectCert != null)
+            {
+                X509CertificateValidator.ChainTrust.Validate(certificate);
+                
+                var subjectUserName = subjectCert.ExtractCommonName();
+                if (!username?.Equals(subjectUserName) ?? false)
+                {
+                    throw new SecurityTokenValidationException("User not valid");
+                }
+                return;
+            }
+
+            if (HttpContext.Current.Request.Headers[SslClientVerifyHeader]?.Equals("0") ?? false)
+            {
+                throw new SecurityTokenValidationException("Certificate not found");
+            }
+
+            if (HttpContext.Current.Request[Sha1HeaderKey]?.Equals(username) ?? false)
+            {
+                throw new SecurityTokenValidationException("User not valid");
+            }
+
+        }
+    }
+
+
+}
